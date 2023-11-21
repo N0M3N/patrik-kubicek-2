406d2a7a1624e310cfd74968b7ea06f3930bf99f
Patrik Kubíček <patrik.kubicek@vyvojari-uo.cz>
2023-11-21 17:04:04 +0100

    change links to routerLinks

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -3,11 +3,12 @@ import { CommonModule } from '@angular/common';
 import { CardsComponent } from '../../components/cards/cards.component';
 import { CardComponent } from '../../components/card/card.component';
 import { LocalizePipe } from '../../pipes/localize.pipe';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [CommonModule, CardsComponent, CardComponent, LocalizePipe],
+  imports: [CommonModule, RouterModule, CardsComponent, CardComponent, LocalizePipe],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
