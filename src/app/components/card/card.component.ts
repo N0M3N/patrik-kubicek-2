1e2c746fc8a1aa3d5b3c18a526b66d20e06322d6
Patrik Kubíček <patrik.kubicek@vyvojari-uo.cz>
2023-11-22 22:06:17 +0100

    responsive

--- src/app/components/card/card.component.ts
+++ src/app/components/card/card.component.ts
@@ -1,21 +1,26 @@
-import { Component, EventEmitter, HostListener, Input, Signal, signal } from '@angular/core';
+import { Component, EventEmitter, Input, Signal, signal } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FaIconComponent } from '@fortawesome/angular-fontawesome';
+import { faChevronDown } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-card',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, FaIconComponent],
   templateUrl: './card.component.html',
   styleUrl: './card.component.scss'
 })
 export class CardComponent {
   @Input() opened: boolean = false;
 
+  faChevronDown = faChevronDown;
+
   public order: number = 0;
   _opened: Signal<boolean> = signal(false);
   onToggle = new EventEmitter();
   
   _onToggle() {
+    console.log('toggled')
     this.onToggle.emit();
   }
 }
