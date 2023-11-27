8733a09cdea79354a03bf1f32a5353a8567d77e9
Patrik Kubíček <patrik.kubicek@vyvojari-uo.cz>
2023-11-27 15:04:24 +0100

    add pwa

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,15 +1,21 @@
-import { ApplicationConfig } from '@angular/core';
+import { ApplicationConfig, isDevMode } from '@angular/core';
 import { provideRouter, withInMemoryScrolling } from '@angular/router';
 
 import { routes } from './app.routes';
 import { LocalizationService } from './services/localization.service';
 import { provideHttpClient, withFetch } from '@angular/common/http';
 import { LocalizePipe } from './pipes/localize.pipe';
+import { provideServiceWorker } from '@angular/service-worker';
 
 export const appConfig: ApplicationConfig = {
-  providers: [
-    provideRouter(routes, withInMemoryScrolling({ scrollPositionRestoration: 'top' })),
-    provideHttpClient(withFetch()),
-    LocalizationService,
-    LocalizePipe],
+    providers: [
+        provideRouter(routes, withInMemoryScrolling({ scrollPositionRestoration: 'top' })),
+        provideHttpClient(withFetch()),
+        LocalizationService,
+        LocalizePipe,
+        provideServiceWorker('ngsw-worker.js', {
+            enabled: !isDevMode(),
+            registrationStrategy: 'registerWhenStable:30000'
+        })
+    ],
 };
